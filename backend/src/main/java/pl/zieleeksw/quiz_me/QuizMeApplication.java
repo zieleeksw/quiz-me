a9cf47e40c558821bea37d8f14d15c9f7a264407
zieleeksw <szielinski1508@wp.pl>
2026-03-23 18:32:08 +0100

    Initialize spring boot app

---
+++ backend/src/main/java/pl/zieleeksw/quiz_me/QuizMeApplication.java
@@ -0,0 +1,13 @@
+package pl.zieleeksw.quiz_me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizMeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/pl/zieleeksw/quiz_me/QuizMeApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.zieleeksw.quiz_me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
